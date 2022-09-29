a54cf299dad044b267cd3cb6002448238717f3d9
FrancoAguayo <faguayo@iiconcepcion.edu.ar>
2022-09-29 19:34:23 -0300

    Add files

--- app/src/main/java/com/example/trabajointegrador/CentroMedico/Persona.kt
+++ app/src/main/java/com/example/trabajointegrador/CentroMedico/Persona.kt
@@ -1,4 +1,15 @@
 package com.example.trabajointegrador.CentroMedico
 
 class Persona {
+    var nombre: String=""
+    var apellido: String=""
+    var edad: Int=0
+    var dni: Int=0
+
+    constructor(nombre: String,apellido: String,edad: Int,dni: Int){
+        this.nombre=nombre
+        this.apellido=apellido
+        this.edad=edad
+        this.dni=dni
+    }
 }
