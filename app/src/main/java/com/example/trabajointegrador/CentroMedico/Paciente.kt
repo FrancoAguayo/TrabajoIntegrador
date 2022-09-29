b7f22f9632c83e4961a55166d806c5c720c7fb74
FrancoAguayo <faguayo@iiconcepcion.edu.ar>
2022-09-29 20:19:29 -0300

    Add files

--- app/src/main/java/com/example/trabajointegrador/CentroMedico/Paciente.kt
+++ app/src/main/java/com/example/trabajointegrador/CentroMedico/Paciente.kt
@@ -1,5 +1,26 @@
 package com.example.trabajointegrador.CentroMedico
 
 class Paciente() : Persona(nombre = "", apellido = "", edad = 0, dni = 0) {
+    var peso: Double=0.0
+    var altura: Double=0.0
+    var diagnostico: String=""
 
+    constructor(nombre: String,apellido: String,edad: Int,dni: Int,peso: Double,altura: Double
+    ,diagnostico: String): this (){
+        this.nombre=nombre
+        this.apellido=apellido
+        this.edad=edad
+        this.dni=dni
+        this.peso=peso
+        this.altura=altura
+        this.diagnostico=diagnostico
+    }
+
+    override fun asistirAlCentro() {
+        println("El paciente ${this.nombre} ${this.apellido} entró al centro médico.")
+    }
+
+    override fun toString(): String {
+        return ("El paciente ${this.nombre} ${this.apellido} se encuentra atendido.")
+    }
 }
--- app/src/main/java/com/example/trabajointegrador/CentroMedico/Principal.kt
+++ app/src/main/java/com/example/trabajointegrador/CentroMedico/Principal.kt
@@ -1,4 +1,9 @@
 package com.example.trabajointegrador.CentroMedico
-
+import java.util.Scanner
 fun main() {
+    val read= Scanner(System.`in`)
+    var persona1 = Persona("","",0,0)
+    println("Ingresa el nombre del paciente")
+    persona1.nombre=read.next()
+    println(persona1.nombre)
 }
