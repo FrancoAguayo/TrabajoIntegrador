f7301d6e155baefad938e2f5dc4c53ae1b136622
FrancoAguayo <faguayo@iiconcepcion.edu.ar>
2022-09-30 19:16:53 -0300

    Add files

--- app/src/main/java/com/example/trabajointegrador/CentroMedico/Cirujano.kt
+++ app/src/main/java/com/example/trabajointegrador/CentroMedico/Cirujano.kt
@@ -1,4 +1,20 @@
 package com.example.trabajointegrador.CentroMedico
 
 class Cirujano() : Persona(nombre = "", apellido = "", edad = 0, dni = 0) {
+    var operacion: String=""
+
+    constructor(nombre: String,apellido: String,edad: Int,dni: Int,operacion: String): this(){
+        this.nombre=nombre
+        this.apellido=apellido
+        this.edad=edad
+        this.dni=dni
+        this.operacion=operacion
+    }
+    override fun asistirAlCentro() {
+        println("El cirujano ${this.nombre} ${this.apellido} está entrando al centro médico.")
+    }
+
+    override fun cirugia() {
+        println("El cirujano ${this.nombre} ${this.apellido} va a hacer una cirugía.")
+    }
 }
