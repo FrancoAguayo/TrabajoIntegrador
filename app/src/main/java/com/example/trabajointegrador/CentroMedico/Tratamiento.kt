8027c81de9161a351402a07f0211dfe13dd09a1a
FrancoAguayo <faguayo@iiconcepcion.edu.ar>
2022-10-04 14:28:54 -0300

    Add files

--- app/src/main/java/com/example/trabajointegrador/CentroMedico/Tratamiento.kt
+++ app/src/main/java/com/example/trabajointegrador/CentroMedico/Tratamiento.kt
@@ -2,11 +2,23 @@ package com.example.trabajointegrador.CentroMedico
 
 class Tratamiento {
     var tipoTratamiento: String=""
+    var duracion: Int=0
     var requiereCirugia: Boolean=false
-    var requiereMedicacion: Boolean=false
+    var frecuenciaMedicacion: Int=0
     var paciente: Paciente
 
     constructor(paciente: Paciente){
         this.paciente = paciente
+    }
+
+    fun tratamiento(tipoTratamiento: String, duracion: Int) : String{
+        return ("El paciente está haciendo un tratamiento de tipo $tipoTratamiento.")
+    }
+    fun tratamiento(tipoTratamiento: String,duracion: Int,requiereCirugia: Boolean): String{
+        return ("El paciente está haciendo un tratamiento de tipo $tipoTratamiento involucró una cirugía urgente.")
+    }
+    fun tratamiento(tipoTratamiento: String,duracion: Int,frecuenciaMedicacion: Int): String{
+        return ("El paciente está haciendo un tratamiento de tipo $tipoTratamiento que involucra una medicación." +
+                "La medicacion se toma cada $frecuenciaMedicacion horas.")
     }
 }
