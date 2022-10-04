992834d8210f7e8b26914f406819b51925f8bb51
FrancoAguayo <faguayo@iiconcepcion.edu.ar>
2022-10-04 20:48:18 -0300

    Add files

--- app/src/main/java/com/example/trabajointegrador/CentroMedico/Principal.kt
+++ app/src/main/java/com/example/trabajointegrador/CentroMedico/Principal.kt
@@ -5,7 +5,8 @@ fun main() {
     var paciente1 = Paciente("","",0,0,0.0,0.0,"")
     var medico1 = Medico("","",0,0,0,"")
     var cirujano1= Cirujano("","",0,0,"")
-    var paciente2 = Paciente("Daniel","Hernandez",34,45272572,56.8,1.54,"bronquiolitis")
+    var paciente2 = Paciente("Daniel","Hernandez",34,
+        45272572,56.8,1.54,"bronquiolitis")
     var tratamiento1= Tratamiento(paciente1)
     var tratamiento2= Tratamiento(paciente2)
     println("Ingresa el nombre del paciente.")
@@ -53,6 +54,12 @@ fun main() {
     println("Ingrese la operación a ejecutar del cirujano.")
     cirujano1.operacion=read.next()
     cirujano1.cirugia()
-    tratamiento1.tratamiento("Reposo",3,true)
-    tratamiento2.tratamiento("Farmacoterapia",5,4)
+    println("************************************")
+    if(paciente1.diagnostico!="apendicitis"){
+        tratamiento1.tratamiento("Terapia Intesiva",6)
+    }
+    else{
+        tratamiento1.tratamiento("Reposo",3,"Apendicetomia")
+    }
+
 }
--- app/src/main/java/com/example/trabajointegrador/CentroMedico/Tratamiento.kt
+++ app/src/main/java/com/example/trabajointegrador/CentroMedico/Tratamiento.kt
@@ -3,7 +3,7 @@ package com.example.trabajointegrador.CentroMedico
 class Tratamiento {
     var tipoTratamiento: String=""
     var duracion: Int=0
-    var requiereCirugia: Boolean=false
+    var tipoCirugia: String=""
     var frecuenciaMedicacion: Int=0
     var paciente: Paciente
 
@@ -11,14 +11,22 @@ class Tratamiento {
         this.paciente = paciente
     }
 
-    fun tratamiento(tipoTratamiento: String, duracion: Int,paciente: Paciente){
+    fun tratamiento(tipoTratamiento: String, duracion: Int){
+        this.tipoTratamiento=tipoTratamiento
+        this.duracion=duracion
         println("El paciente ${paciente.nombre} ${paciente.apellido} está haciendo un tratamiento de tipo $tipoTratamiento con duracion de $duracion mes/es.")
     }
-    fun tratamiento(tipoTratamiento: String,duracion: Int,requiereCirugia: Boolean){
-        println("El paciente ${paciente.nombre} ${paciente.apellido} está haciendo un tratamiento de tipo $tipoTratamiento que involucró una cirugía urgente la" +
-                " cual duró $duracion horas.")
+    fun tratamiento(tipoTratamiento: String,duracion: Int,tipoCirugia: String){
+        this.tipoTratamiento=tipoTratamiento
+        this.duracion=duracion
+        this.tipoCirugia=tipoCirugia
+        println("El paciente ${paciente.nombre} ${paciente.apellido} está haciendo un tratamiento de tipo $tipoTratamiento que" +
+                " involucró una cirugía urgente de tipo $tipoCirugia la cual duró $duracion horas.")
     }
     fun tratamiento(tipoTratamiento: String,duracion: Int,frecuenciaMedicacion: Int){
+        this.tipoTratamiento=tipoTratamiento
+        this.duracion=duracion
+        this.frecuenciaMedicacion=frecuenciaMedicacion
         println("El paciente ${paciente.nombre} ${paciente.apellido} está haciendo un tratamiento de tipo $tipoTratamiento que involucra una medicación." +
                 "La medicacion se toma cada $frecuenciaMedicacion horas por $duracion mes/es.")
     }
